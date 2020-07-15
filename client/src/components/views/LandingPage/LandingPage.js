557d0f717035871b30c4683c97c0965823a39760
정원형 <spowide@jeong-wonhyeong-ui-MacBookPro.local>
2020-07-15 17:49:05 +0900

    로그아웃

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,17 +1,31 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
-const LandingPage = () => {
+const LandingPage = (props) => {
 
     useEffect(() => {
         axios.get('/api/hello')
-            .then(response => { console.log(response) })
+            .then(response => {
+
+            })
     }, [])
+
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                if (response.data.success) {
+                    props.history.push("/login");
+                } else {
+                    alert('Failed Logout');
+                }
+            })
+    }
 
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
         }}>
             <h2>시작페이지</h2>
+            <button onClick={onClickHandler}>Logout</button>
         </div >
     )
 }
